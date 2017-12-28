ff4f0638ecf6011da37d2c96e413c29c78f906a7
Adrian-PC\Adrian <a.chmilowski@interia.pl>
2017-12-28 14:44:23 +0100

    Initial commit

---
+++ ChromeTest/src/ChromeTest/Chrome.java
@@ -0,0 +1,16 @@
+package Chrome;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrow {
+	
+	public static void main(String[]args) {
+
+	WebDriver driver = new ChromeDriver();	
+	
+	driver.get("www.google.pl");
+
+	
+	}
+}
